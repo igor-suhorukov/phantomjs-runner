080b2957318b60369d52b65e75fecd5e7fd16b8c
igor-suhorukov <igor.suhorukov@gmail.com>
2017-03-05 20:20:09 +0300

    update maven-embedder version, attach maven runner script

---
+++ src/main/resources/MavenRunner.java
@@ -0,0 +1,9 @@
+//dependency:mvn:/com.github.igor-suhorukov:phantomjs-runner:${project.version}
+//dependency:mvn:/org.slf4j:slf4j-simple:1.7.5
+import com.github.igorsuhorukov.maven.MavenUtils;
+
+class MavenRunner {
+    public static void main(String[] args) throws Exception{
+        System.exit(MavenUtils.executeMavenTask(System.getProperty("user.dir"), args, System.out, System.err));
+    }
+}
